04fda82a5b3bf771934e283d095e577a39ff09c3
HAFSSA <hafssa.miftah-etu@etu.univh2c.ma>
2026-05-11 17:03:04 +0100

    Test

--- src/main/java/hafssamiftahidrissi/example/hafssamiftahidrissiexamjeebackend/security/SecurityController.java
+++ src/main/java/hafssamiftahidrissi/example/hafssamiftahidrissiexamjeebackend/security/SecurityController.java
@@ -1,4 +1,66 @@
 package hafssamiftahidrissi.example.hafssamiftahidrissiexamjeebackend.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
 public class SecurityController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtEncoder jwtEncoder;
+
+    public SecurityController(AuthenticationManager authenticationManager, JwtEncoder jwtEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(
+            @RequestParam String username,
+            @RequestParam String password
+    ) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+
+        Instant instant = Instant.now();
+
+        String scope = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuedAt(instant)
+                .expiresAt(instant.plus(10, ChronoUnit.MINUTES))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(
+                JwsHeader.with(MacAlgorithm.HS512).build(),
+                jwtClaimsSet
+        );
+
+        String token = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+
+        return Map.of("access-token", token);
+    }
 }
