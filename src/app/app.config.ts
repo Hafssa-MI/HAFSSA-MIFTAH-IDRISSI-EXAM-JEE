ca4050c93ebaaef9772a7b00cdb9c3b6e7c89ff1
HAFSSA <hafssa.miftah-etu@etu.univh2c.ma>
2026-05-11 14:11:25 +0100

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(ReactiveFormsModule)
+  ]
+};
---
+++ src/app/new-location/new-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-location',
+  imports: [],
+  templateUrl: './new-location.component.html',
+  styleUrl: './new-location.component.css'
+})
+export class NewLocationComponent {
+
+}
